9dfcb5e6acb39b783fa051c5b2e3e5d621593a64
JessieYan123 <45013145+JessieYan123@users.noreply.github.com>
2019-06-09 10:44:35 -0700

    Update slow_style_utils.py

--- SlowStyleTransfer/slow_style_utils.py
+++ SlowStyleTransfer/slow_style_utils.py
@@ -18,7 +18,8 @@ import copy
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 def LoadImage(img_name, device):
-	im_size = 512 if torch.cuda.is_available() else 128  # use small size if no gpu
+	# load size based on whether gpu is available
+	im_size = 512 if torch.cuda.is_available() else 128  
 	loader = transforms.Compose([transforms.Resize(im_size),  
 									transforms.ToTensor()])  
 	img = Image.open(img_name)
@@ -27,9 +28,10 @@ def LoadImage(img_name, device):
 	return img.to(device, torch.float)
 
 def SaveImage(tensor, args):
-	unloader = transforms.ToPILImage()  # reconvert into PIL img
-	img = tensor.cpu().clone()  # we clone the tensor to not do changes on it
-	img = img.squeeze(0)      # remove the fake batch dimension
+	# transfer, clone and remove fake dimension
+	unloader = transforms.ToPILImage()  
+	img = tensor.cpu().clone()  
+	img = img.squeeze(0)      
 	img = unloader(img)
 	if args.test_mode == True:
 		opt_path = "SlowStyleTransfer/OutputImages/{0}.jpg".format(args.output_name)
@@ -38,6 +40,7 @@ def SaveImage(tensor, args):
 		img.save(args.output_image)
 
 def PlotImage(args):
+	# plot image
 	plt.figure(figsize=(15,15))
 	paths = [args.style_image, args.content_image, args.output_image]
 	title = ["Style Image", "Content Image", "Output Image"]
@@ -59,15 +62,18 @@ def PlotImage(args):
         
 
 class ContentLoss(nn.Module):
+	# detach content from image
 	def __init__(self, target,):
 		super(ContentLoss, self).__init__()
 		self.target = target.detach()
 
 	def forward(self, input):
+		# throw error if content is a variable
 		self.loss = F.mse_loss(input, self.target)
 		return input
 
 class StyleLoss(nn.Module):
+	# detach style from image
 	def __init__(self, target_feature):
 		super(StyleLoss, self).__init__()
 		self.target = self.gram_matrix(target_feature).detach()
@@ -78,18 +84,16 @@ class StyleLoss(nn.Module):
 		return input
 
 	def gram_matrix(self, input):
-		a, b, c, d = input.size()  # a=batch size(=1)
-		# b=number of feature maps
-		# (c,d)=dimensions of a f. map (N=c*d)
-
-		features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
-		G = torch.mm(features, features.t())  # compute the gram product
-
-		# we 'normalize' the values of the gram matrix
-		# by dividing by the number of element in each feature maps.
+		# transfer iamge into gram matrix
+		a, b, c, d = input.size()  
+		features = input.view(a * b, c * d)  
+		# compute the gram product
+		G = torch.mm(features, features.t())  
+		# normalize the values of G
 		return G.div(a * b * c * d)
 
 class Normalization(nn.Module):
+	# normalize image to fit the Tensor
 	def __init__(self, mean, std):
 		super(Normalization, self).__init__()
 		self.mean = mean.clone().detach().view(-1, 1, 1)
